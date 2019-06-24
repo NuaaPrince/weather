066e29757476eb616794f14aaf8687815f137b79
Xavier Wang <1209111710@qq.com>
2019-06-25 00:38:21 +0800

    新增配置中心

--- weather-report-server/src/test/java/com/xavier/springboot/weather/WeatherApplicationTests.java
+++ weather-report-server/src/test/java/com/xavier/springboot/weather/WeatherApplicationTests.java
@@ -2,15 +2,20 @@ package com.xavier.springboot.weather;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WeatherApplicationTests {
-
+    @Value("${auther}")
+    private String auther;
     @Test
     public void contextLoads() {
+        assertEquals("xavier",auther);
     }
 
 }
